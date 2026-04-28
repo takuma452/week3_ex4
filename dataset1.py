9b3c6467e00d641c58226041ad3ad3a5e4927a46
Uehara Takuma <e245743@ie.u-ryukyu.ac.jp>
2026-04-28 11:31:38 +0900

    ノイズありの観測値を実装

--- dataset1.py
+++ dataset1.py
@@ -23,8 +23,15 @@ df = pd.DataFrame({
     '観測点': x_obs,
     '真値': y_true
 })
-# 確認
-print(df)
+
+# ノイズを生成
+std_dev = np.sqrt(2.0)
+raw_noise = np.random.normal(loc=0.0, scale=std_dev, size=n)
+
+noise = raw_noise / 2.0
+
+# ノイズを加えた観測値をDataFrameに追加
+df['観測値'] = df['真値'] + noise
 
 x = np.linspace(-1, 1, 100)
 y = true_function(x)
@@ -32,8 +39,11 @@ y = true_function(x)
 plt.figure(figsize=(10, 5))
 plt.plot(x, y , label='y = 10 * sin(0.8 * pi * x)', color='blue')
 
-# 観測点をプロット
+# 観測点と真値をプロット
 plt.scatter(df['観測点'], df['真値'], color='red', label='観測点', marker='o')
+# 観測点と観測値をプロット
+plt.scatter(df['観測点'], df['観測値'], color='green', label='観測値', marker='x')
+
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('真値と観測点のプロット')
@@ -41,5 +51,5 @@ plt.grid(True)
 plt.legend()
 
 # 画像として保存
-plt.savefig('image/ex1.2.png')
+plt.savefig('image/ex1.3.png')
 plt.show()
