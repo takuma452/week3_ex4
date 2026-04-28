c441b8caa131fc28cc575c5ea655088f5efaf629
Uehara Takuma <e245743@ie.u-ryukyu.ac.jp>
2026-04-28 13:07:25 +0900

    演習1.1~1.5をそれぞれ関数化

--- dataset1.py
+++ dataset1.py
@@ -9,26 +9,66 @@ def true_function(x):
 0.0
   """
   return np.sin(np.pi * x * 0.8) * 10
-# 乱数のシード値を設定
-np.random.seed(0)
 
-# 乱数の個数
-n = 20
+def generate_dataset(n):
+  """
+  >>> df = generate_dataset(5)
+  >>> len(df)
+  5
+  """
+  # 乱数のシード値を設定
+  np.random.seed(0)
 
-x_obs = np.random.uniform(-1, 1, n)
-y_true = true_function(x_obs)
+  # 乱数の個数
+  n = 20
 
+  # 観測点を生成
+  x_obs = np.random.uniform(-1, 1, n)
+  y_true = true_function(x_obs)
 
-df = pd.read_csv('dataset/dataset1.tsv', sep='\t')
+  # DataFrameにデータを格納
+  df = pd.DataFrame({
+      '観測点': x_obs,
+      '真値': y_true
+  })
+  
+  return df
 
-# ノイズを生成
-std_dev = np.sqrt(2.0)
-raw_noise = np.random.normal(loc=0.0, scale=std_dev, size=n)
+def generate_noisy_dataset(n):
+  """
+  >>> df = generate_noisy_dataset(5)
+  >>> len(df) 
+  5
+  """
+  df = generate_dataset(n)
+  # ノイズを生成
+  std_dev = np.sqrt(2.0)
+  raw_noise = np.random.normal(loc=0.0, scale=std_dev, size=n)
 
-noise = raw_noise / 2.0
+  noise = raw_noise / 2.0
 
-# ノイズを加えた観測値をDataFrameに追加
-df['観測値'] = df['真値'] + noise
+  # ノイズを加えた観測値をDataFrameに追加
+  df['観測値'] = df['真値'] + noise
+  
+  return df
+
+def export_to_TSV(df, filename):
+  """
+  >>> df = generate_noisy_dataset(5)
+  >>> export_to_TSV(df, 'test.tsv')
+  >>> import os
+  >>> os.path.exists('test.tsv')
+  True
+  """
+  return df.to_csv('dataset/' + filename, sep='\t', index=False)
+
+def import_TSV(filename):
+  """
+  >>> df = inport_TSV('dataset1.tsv')
+  >>> len(df)
+  20
+  """
+  return pd.read_csv('dataset/' + filename, sep='\t')
 
 x = np.linspace(-1, 1, 100)
 y = true_function(x)
@@ -50,6 +90,3 @@ plt.legend()
 # 画像として保存
 plt.savefig('image/ex1.3.png')
 plt.show()
-
-# データセットをTSV形式で保存
-df.to_csv('dataset/dataset1.tsv', sep='\t', index=False)
